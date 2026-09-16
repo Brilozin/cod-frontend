a9c7b9e2c763aebd7e7c73e2609cdc80e9fd72ef
CRISTIANO DOS SANTOS DIAS FILHO <cristiano_dias-fi@estudante.sesisenai.org.br>
2026-09-16 14:44:41 -0300

    Atualiza fonte do projeto

--- 00-demo/src/App.jsx
+++ 00-demo/src/App.jsx
@@ -1,41 +1,51 @@
 import './App.css'
 
 function App() {
-
-
+ 
   return (
-    <div className="cont-app">
-      <h1>Trampando com React</h1>
-      <h2>Onde posso trampa como dev</h2>
-      <p>
-        Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.
-      </p>
-      <p>
-        Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.
-      </p>
-      <p>
-        Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que mantêm equipes internas para cuidar de seus portais e sistemas.
-      </p>
-      <p>
-        Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.
-      </p>
-      <p>
-        Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.
-      </p>
+  <div className="cont-app">
+    <h1>TRABALHANDO COM REACT</h1>
+    <h2>ONDE POSSO TRAMPAR COMO DEV</h2>
+    <ul>
+  <li><strong>Empresas e Software Houses:</strong> Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.</li>
+  <br />
+  <li><strong>Startups de Tecnologia:</strong> Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.</li>
+  <br />
+  <li><strong>Departamentos de TI e Inovação:</strong> Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que gerenciam equipes internacionais para cuidar de seus portais e sistemas.</li>
+  <br />
+  <li><strong>Agências Digitais e de Marketing:</strong> Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.</li>
+  <br />
+  <li><strong>Setor Público:</strong> Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.</li>
+</ul>
 
-      <h2>Trampos de tecnologia mas sem programação</h2>
-      <p>
-        Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócios.
-      </p>
-
-      <ul>
-        <li>Product Management & Negócios</li>
-        <li>Design e Experiência do Usuário (UI/UX)</li>
-        <li>Gestão de Projetos e Agilidade</li>
-        <li>Dados e Inteligência de Negócio</li>
-        <li>Vendas Técnicas e Sucesso do Cliente</li>
-      </ul>
-    </div>
+     <h2>TRAMPO COM TECNOLOGIA SEM PROGRAMAÇÃO</h2>
+     <ul>
+     <li>Product Management & Negócios</li>
+     <br />
+     <li>Design e Experiência do Usuário (UI/UX)</li>
+     <br />
+     <li>Gestão de Projetos e Agilidade</li>
+     <br />
+     <li>Dados e Inteligência de Negócio</li>
+     <br />
+     <li>Vendas Técnicas e Sucesso do Cliente</li>
+     <br />
+     </ul>
+     
+     <h2>VAI FAZER FACULDADE DE OUTRA COISA 
+       </h2>
+     <ul>
+     <li><strong>Direito:</strong> Resumos doutrinários, peças jurídicas e simulados.</li>
+     <br />
+     <li><strong>Saúde:</strong> Explicação de anatomia/fisiologia, artigos e casos clínicos.</li>
+     <br />
+     <li><strong>Negócios:</strong> Planos de negócios, análise SWOT e cálculos econômicos.</li>
+     <br />
+     <li><strong>Humanas:</strong> Resenhas, comparação de autores e normas ABNT.</li>
+     <br />
+     <li><strong>Engenharias:</strong> Passo a passo de cálculo, física e relatórios.</li>
+     </ul>
+  </div>
   )
 }
 

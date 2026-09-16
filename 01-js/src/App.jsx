ce57f08204450fd14727ff35d82fe61881629aec
CRISTIANO DOS SANTOS DIAS FILHO <cristiano_dias-fi@estudante.sesisenai.org.br>
2026-09-16 16:05:13 -0300

    Adiciona exercicio de força de trabalho

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -1,46 +1,86 @@
 import './App.css'
 
 function App() {
-  function calcularPontos(){
+
+  function calcularDevs() {
+    let clt = Number(prompt("Quantos devs CLT?"))
+
+    let estagiarios = Number(prompt("Quantos devs estagiários?"))
+
+    let pj = Number(prompt("Quantos devs PJ?"))
+
+    let total = clt + estagiarios + pj
+
+    alert("A empresa tem " + total + " devs no total.")
+  }
+
+  function calcularPontos() {
     let vitorias = Number(prompt("Quantas vitórias teve seu time: "))
+
     let empates = Number(prompt("Quantos empates teve seu time: "))
-    let pontos = vitorias*3 + empates*1
-    alert( 'O seu time tem: ' + pontos +' pontos')
 
+    let pontos = vitorias * 3 + empates * 1
 
+    alert('O seu time tem: ' + pontos + ' pontos')
   }
-  function trocraCalcados(){
+
+  function trocarCalcados() {
     let precoCalcado = Number(prompt('Qual o preço do Calçado: '))
-    let quantidadePares = Number(prompt("Qual aquantidade: "))
-    let valeTrocas = precoCalcado *quantidadePares
-    alert('A empresa vai recber em vales trocas: ' + valeTrocas + ' reais')
+
+    let quantidadePares = Number(prompt("Qual a quantidade: "))
+
+    let valeTrocas = precoCalcado * quantidadePares
+
+    alert('A empresa vai receber em vales trocas: ' + valeTrocas + ' reais')
   }
- function testar (){
- let nome = prompt('Qual o seu nome: ')
- let bocaDoSapo = nome
- alert(nome + ', seu nome está na boca do sapo 🐸 ')
- }
+
+  function testar() {
+    let nome = prompt('Qual o seu nome: ')
+
+    alert(nome + ', seu nome está na boca do sapo 🐸')
+  }
+
+  function calcularMedia() {
+    let nota1 = Number(prompt("Qual o valor da primeira nota:"))
+
+    let nota2 = Number(prompt("Qual o valor da segunda nota:"))
+
+    let media = (nota1 + nota2) / 2
+
+    alert('Sua média final: ' + media)
+  }
 
   return (
     <div className="cont-app">
-    <h1>JavaScript no React</h1>
-    <h2>Exercicios Sumpipas</h2>
 
-    <button onClick={calcularPontos}>campeonato</button>
-    <button onClick={trocraCalcados}>Trocas Pé Pequeno</button>
-    <hr />
-    <button onClick={testar}>Testar</button>
-    <button onClick={calcularMedia}>Média</button>
-    
+      <h1>JavaScript no React</h1>
+
+      <h2>Exercícios Surpresa</h2>
+
+      <button onClick={calcularDevs}>
+        Força de trabalho
+      </button>
+
+      <button onClick={calcularPontos}>
+        Campeonato
+      </button>
+
+      <button onClick={trocarCalcados}>
+        Trocas Pé Pequeno
+      </button>
+
+      <hr />
+
+      <button onClick={testar}>
+        Testar
+      </button>
+
+      <button onClick={calcularMedia}>
+        Média
+      </button>
+
     </div>
   )
-}
-
-function calcularMedia (){
-let nota1 = Number((prompt("Qual o valor da primeira nota:")))
-let nota2 = Number((prompt("Qual o valor da segunda nota:")))
-let media = (nota1 + nota2) /2;
-alert('Sua média final: ' + media)
 }
 
 export default App
